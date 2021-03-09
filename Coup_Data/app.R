33170bf9a1d70c7d89155d58f7d0840b1a3d2165
Your Name <nanakorantema_koranteng@fas.harvard.edu>
2021-03-09 15:02:54 -0500

    tried to update visual

--- Coup_Data/app.R
+++ Coup_Data/app.R
@@ -1,49 +1,55 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(tidycensus)
+library(tidyverse)
+library(shinythemes)
+source("Plots.R")
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
-)
+# Define UI for application 
+ui <- navbarPage(theme = shinytheme("cyborg"),
+                 "Coups and Democracy in the Middle East",
+                 tabPanel("Discussion",
+                          titlePanel("Project Data"),
+                          h3("Sourcing My Data"),
+                          p("For my project I plan to look at two data sets. I will first look at coup data collected 
+                by The Cline Center for Advanced Social Resaerch. Their data set includes information
+                on coups, attempted coups, and coup plots/conspiracies in 136 countries from 1945-2019.
+                that data specifies the type of actor(s) who initiated the coup plots and information 
+                regarding what happened to the deposed leader in each case. Being that the so called 
+                Arab Spring has served as a catalyst for civil resistance and political reform in the Middle
+                East, I am hoping to use this data to compare coup attempts in the region and the 
+                rest of the world. 
+                Next, I plan to use an Arab barometer data set entitled Democracy in the Middle East 
+                and North Africa: Five Years after the Arab Uprisings which explores opinions towards
+                democracy in selected MENA countries. Essentially, I hope to look for some type of relationship
+                between coup attempts in the region and feelings toward democracy. I will deinfitely need to take
+                my time to go through the codebook for this set as the raw survey data uses many codes and scales.
+                The direction of my project might change (and probably will as I take a closer look at the data)
+                but I am excited about using two different types of data sets that will provide a meta and micro 
+                level study of a region I am deeply interested in.")),
+                 tabPanel("Visualing the Data", 
+                          titlePanel("1st Plot"),
+                          h3("Project Background and Motivations"),
+                          p("Hello, this is a shiny app that I have created for milestone #3 of my big data course"),
+                          p("My name is Nana-Korantema Koranteng and I study the Middle East.")),
+                        mainPanel(plotOutput("plots")),
+                 tabPanel("About", 
+                          titlePanel("About"),
+                          h3("Project Background and Motivations"),
+                          p("Hello, this is a shiny app that I have created for milestone #3 of my big data course"),
+                          h3("About Me"),
+                          p("My name is Nana-Korantema Koranteng and I study the Middle East. 
+                            You can reach me at nanakorantema_koranteng@g.harvard.edu.")))
 
 # Define server logic required to draw a histogram
+
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    
+    output$plot <- renderPlot({
+        if(input$plot_type == "a"){            
+            plot_1
+        }                                        
+        
     })
 }
-
 # Run the application 
 shinyApp(ui = ui, server = server)
---
+++ Plots.R
@@ -0,0 +1,12 @@
+plot_1 <- top_10_coups %>% 
+  ggplot(aes(x = fct_reorder(country, total_coup),
+             y = total_coup)) +
+  labs(title = "Top 10 Countries with the Most Successful Coups from 1945- 2019",
+       subtitle = "South and Central American countires have had the most successful coups",
+       caption = "Source: Cline Center Coup D’état Project Dataset",
+       x = "Country",
+       y = "Number of Successful Coups") +
+  coord_flip() +
+  theme_minimal() +
+  geom_col(fill = "darkolivegreen4")
+
