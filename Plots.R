778ad401a9400878416accfc0bce76fbb7d069c0
Your Name <nanakorantema_koranteng@fas.harvard.edu>
2021-04-08 16:30:53 -0400

    started updating maps to highercharter

--- Plots.R
+++ Plots.R
@@ -3,9 +3,12 @@ library(primer.data)
 library(tidyverse)
 library(ggplot2)
 library(shinythemes)
+library(highcharter)
+library(dplyr)
+library(maps)
+library(tidyverse)
 library(ggplot2)
 library(janitor)
-library(rworldmap)
 
 coup_data <- read_csv(file = "Coup_Data_v2.0.0.csv")
 
@@ -51,61 +54,6 @@ plot_2 <- attempted_coups %>%
 
 #Mapping info
 
-Clean_coup <- coup_data %>% 
-  select( - c(realized, unrealized, conspiracy, attempt, coup_id)) %>% 
-  group_by(year) %>% 
-  arrange(desc(year))
 
-
-grouped_coup <- Clean_coup %>% 
-  group_by(country, event_type) %>% 
-  summarise(Coups = n()) %>% 
-  filter(event_type == "coup")
-
-
-joined_data <-joinCountryData2Map(grouped_coup,
-                                  joinCode = "NAME",
-                                  nameJoinColumn = "country")
-
-thecoupMap <- mapCountryData( joined_data, nameColumnToPlot="Coups",
-                              missingCountryCol='dark grey',
-                              oceanCol="light blue",
-                              addLegend=TRUE )
-mtext("[Grey Color: No Data Available]",side=1,line=-1)
-
-
-grouped_attempted <- Clean_coup %>% 
-  group_by(country, event_type) %>% 
-  summarise(Attempts = n()) %>% 
-  filter(event_type == "attempted")
-
-
-joined_data <-joinCountryData2Map(grouped_attempted,
-                                  joinCode = "NAME",
-                                  nameJoinColumn = "country")
-
-theattemptedMap <- mapCountryData( joined_data, nameColumnToPlot="Attempts",
-                                   missingCountryCol='dark grey',
-                                   oceanCol="light blue",
-                                   addLegend=TRUE )
-mtext("[Grey Color: No Data Available]",side=1,line=-1)
-
-
-grouped_conspiracy <- Clean_coup %>% 
-  group_by(country, event_type) %>% 
-  summarise(Conspiracies = n()) %>% 
-  filter(event_type == "conspiracy")
-
-
-joined_data <-joinCountryData2Map(grouped_conspiracy,
-                                  joinCode = "NAME",
-                                  nameJoinColumn = "country")
-
-theconspiracyMap <- mapCountryData( joined_data, 
-                                    nameColumnToPlot="Conspiracies",
-                                    missingCountryCol='dark grey',
-                                    oceanCol="light blue",
-                                    addLegend=TRUE )
-mtext("[Grey Color: No Data Available]",side=1,line=-1)
 
 
